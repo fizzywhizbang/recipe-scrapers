9b6e6704265bab307588091ae9593a9a0cc3392d
FizzyWhizbang <marc.levine@gmail.com>
2021-05-11 11:27:25 -0500

    added eatingwell
    updated to scrape eatingwell recipes

--- recipe_scrapers/eatingwell.py
+++ recipe_scrapers/eatingwell.py
@@ -3,19 +3,19 @@ from ._utils import get_minutes, normalize_string, dateCleaner
 
 class Eatingwell(JSONScraper):
     @classmethod
+    
     def host(self):
         return 'eatingwell.com'
 
     def title(self):
-        print(self.data[1])
-        return self.data["name"]
+        return self.data[1]["name"]
     #need to figure out something for date published
     def datePublished(self):
         date = dateCleaner("null",6)
         return date
 
     def description(self):
-        return self.data["description"]
+        return self.data[1]["description"]
 
 
     def total_time(self):
@@ -24,7 +24,7 @@ class Eatingwell(JSONScraper):
 
     def ingredients(self):
         ing = ""
-        ingList = self.data['recipeIngredient']
+        ingList = self.data[1]['recipeIngredient']
         i = 0
         while i < len(ingList):
             ing += ingList[i] + "\n"
@@ -34,37 +34,37 @@ class Eatingwell(JSONScraper):
 
     def instructions(self):
         #this is a nested array
-        instrList = self.data['recipeInstructions']
+        instrList = self.data[1]['recipeInstructions']
         i = 0
         instr = ""
         while i < len(instrList):
-            instr += instrList[i] + "\n"
+            instr += instrList[i]['text'] + "\n"
             i += 1
 
         return instr
 
     def category(self):
-        return self.data["recipeCategory"][0]
+        return self.data[1]["recipeCategory"][0]
 
     def imgURL(self):
-        return self.data["image"][3]
+        return self.data[1]["image"]['url']
 
 
     def sodium(self):
-        return self.data["nutrition"]["sodiumContent"]
+        return self.data[1]["nutrition"]["sodiumContent"]
 
     def fat(self):
-        return self.data["nutrition"]["fatContent"]
+        return self.data[1]["nutrition"]["fatContent"]
 
     def carbs(self):
-        return self.data["nutrition"]["carbohydrateContent"]
+        return self.data[1]["nutrition"]["carbohydrateContent"]
 
     def calories(self):
-        return self.data["nutrition"]["calories"]
+        return self.data[1]["nutrition"]["calories"]
 
 
     def cholesterol(self):
-        return self.data["nutrition"]["cholesterol"]
+        return self.data[1]["nutrition"]["cholesterolContent"]
 
     def rawData(self):
-        return self.data
+        return self.data[1]
